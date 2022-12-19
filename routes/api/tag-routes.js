2beb03a268314229522314e4392903c131955b16
alex barlow <alex.barlow@rocketmail.com>
2022-12-19 10:49:23 -0500

    changed get routes in tag to async and created the routes/queries

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,53 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  await Product.findAll({
+		attributes: ["id", "product_name", "price", "stock", "category_id"],
+		include: [
+			{
+				model: Tag,
+				attributes: ["id", "tag_name"],
+				through: "ProductTag",
+			},
+			{
+				model: Category,
+				attributes: ["id", "category_name"],
+			},
+		],
+	})
+		.then((productData) => {
+			res.json(productData);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Product.findByPk(req.params.id, {
+		include: [
+			{
+				model: Tag,
+				attributes: ["id", "tag_name"],
+				through: "ProductTag",
+			},
+			{
+				model: Category,
+				attributes: ["id", "category_name"],
+			},
+		],
+	})
+		.then((specificProduct) => {
+			res.json(specificProduct);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
 });
 
 router.post('/', (req, res) => {
